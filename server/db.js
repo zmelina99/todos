ecdc39c271ece266de1eb9f4f9ae393c4efaa5c1
Melina <72397189+zmelina99@users.noreply.github.com>
2023-01-13 01:20:36 -0300

    added database initial values

--- server/db.js
+++ server/db.js
@@ -1,12 +1,55 @@
-//set up connection between database and server
-
-const Pool = require('pg').Pool;
+const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  port: 5432, 
-  database: 'perntodo'
+  port: 5432,
+  database: 'perntodo',
 });
 
-module.exports = pool
+const categoriesTable = `
+CREATE TABLE IF NOT EXISTS categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(100) NOT NULL,
+    color VARCHAR(100) NOT NULL
+);
+`;
+
+const todosTable = `
+CREATE TABLE IF NOT EXISTS todo (
+    todo_id SERIAL PRIMARY KEY,
+    todo_name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    completed BOOLEAN DEFAULT false,
+    category_id INTEGER REFERENCES categories(category_id)
+);
+`;
+
+const queryString =
+  "INSERT INTO categories (category_name, color) VALUES ('Groceries', 'blue')";
+
+pool.query(categoriesTable, (err, res) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    console.log(res.command + ' : ' + res.rowCount + ' rows');
+
+    pool.query(todosTable, (err, res) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        console.log(res.command + ' : ' + res.rowCount + ' rows');
+
+        pool.query(queryString, (err, res) => {
+          if (err) {
+            console.log(err.stack);
+          } else {
+            console.log(res.command + ' : ' + res.rowCount + ' rows');
+          }
+        });
+      }
+    });
+  }
+});
+
+module.exports = pool;
